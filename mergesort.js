64f67739fbfac7ade3db0bff165fd38ae4ddd079
David Berger <dgaberger@gmail.com>
2017-07-27 16:00:02 -0400

    added faster merge, no shift()

--- mergesort.js
+++ mergesort.js
@@ -12,29 +12,58 @@ merge.split = function(array){
 }
 
 merge.combine = function(array){
-	this.array = array;
 	var result = [];
-	while (array[0][0] && array[1][0]){
-		if (array[0][0] > array[1][0]){
-			result.push(array[1].shift());
-		} else {
-			result.push(array[0].shift())
+
+	//don't shift version
+	var left = array[0],
+	right = array[1],
+
+	leftIdx = 0,
+	rightIdx = 0;
+
+	while (leftIdx < left.length && rightIdx < right.length){
+		if (left[leftIdx] < right[rightIdx]){
+			result.push(left[leftIdx])
+			leftIdx++
+		}
+		else{
+			result.push(right[rightIdx])
+			rightIdx++
 		}
 	}
-	while (array[0][0]){
-		result.push(array[0].shift())
-	} 
-	while (array[1][0]){
-		result.push(array[1].shift())
-	}
+
+	for (; leftIdx < left.length; leftIdx++) result.push(left[leftIdx])
+	for (; rightIdx < right.length; rightIdx++) result.push(right[rightIdx])
+	//end don't shift version
+
+
+
+	// O(n^2) version, sad face
+
+	// while (array[0][0] && array[1][0]){		
+	// 	if (array[0][0] > array[1][0]){
+	// 		result.push(array[1].shift());
+	// 	} else {
+	// 		result.push(array[0].shift())
+	// 	}
+	// }
+	// while (array[0][0]){
+	// 	result.push(array[0].shift())
+	// } 
+	// while (array[1][0]){
+	// 	result.push(array[1].shift())
+	// }
+
+	//end slow version
+
 	return result;
 
 }
 
 merge.sort = function(array){
 	if (array.length < 2) return array;
-	var splitArr = this.split(array)
-	var left = splitArr[0]
-	var right = splitArr[1]
+	var splitArr = this.split(array),
+	left = splitArr[0],
+	right = splitArr[1]
 	return this.combine(	[this.sort(left), this.sort(right)]	)
 }
